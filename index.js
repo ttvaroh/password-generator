3e2da2194aad8ddd09e7b0fb80b2055d66a49e91
ttvaroh <ttvaroh@icloud.com>
2025-08-03 22:28:07 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 const upperCase = characters.filter(c => /[A-Z]/.test(c));
 const lowerCase = characters.filter(c => /[a-z]/.test(c));
@@ -10,36 +10,47 @@ const toggleBtn = document.getElementById('toggle-options');
 const optionsPanel = document.getElementById('options-panel');
 const lengthInput = document.getElementById('password-length');
 const lengthDisplay = document.getElementById('length-display');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
 
 
-genButton.addEventListener('click', function() {
-    password1.innerText = getRandomPassword()
-    password2.innerText = getRandomPassword()
-})
-
-document.querySelectorAll('.res-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        let text = button.innerText;
-        navigator.clipboard.writeText(text)
-            .then(() => {
-                console.log("Copied:", text);
-                button.classList.add("copied");
-                setTimeout(() => button.classList.remove("copied"), 1500);
-            })
-            .catch(err => {
-                console.error("Failed to copy:", err);
-            });
+document.addEventListener("DOMContentLoaded", function () {
+    genButton.addEventListener('click', function() {
+        genButton.classList.add("clicked");
+        setTimeout(() => genButton.classList.remove("clicked"), 200);
+        password1.innerText = getRandomPassword()
+        password2.innerText = getRandomPassword()
+    })
+    
+    document.querySelectorAll('.res-btn').forEach(button => {
+        button.addEventListener('click', () => {
+            let originalText = button.innerText;
+            navigator.clipboard.writeText(originalText)
+                .then(() => {
+                    button.innerText = 'Copied!';
+                    button.classList.add("copied");
+                    button.classList.add('fade');
+                    setTimeout(() => {
+                        button.innerText = originalText;
+                        button.classList.remove("copied")
+                    }, 1500);
+                })
+                .catch(err => {
+                    console.error("Failed to copy:", err);
+                });
+        });
+    });
+    
+    toggleBtn.addEventListener('click', () => {
+        const isHidden = optionsPanel.classList.toggle('hidden');
+        toggleBtn.textContent = isHidden ? 'Show Options' : 'Hide Options';
+    });
+    
+    lengthInput.addEventListener('input', () => {
+      lengthDisplay.textContent = lengthInput.value;
     });
 });
 
-toggleBtn.addEventListener('click', () => {
-    const isHidden = optionsPanel.classList.toggle('hidden');
-    toggleBtn.textContent = isHidden ? 'Show Options ▼' : 'Hide Options ▲';
-});
-
-lengthInput.addEventListener('input', () => {
-  lengthDisplay.textContent = lengthInput.value;
-});
 
 function copyButtonText(button) {
     let text = button.innerText
@@ -60,7 +71,6 @@ function getRandomPassword() {
     let includeSymbols = document.getElementById('include-symbols').checked;
     let passLen = parseInt(document.getElementById('password-length').value, 10);
     let pass = ''
-    
     
     let allowedChars = [...lowerCase];
     if (includeUppercase) allowedChars = allowedChars.concat(upperCase);
